2b1cb0ae932ce1537a6a49e74c1b0ccf24b674e9
studTon <evertonsdasilva1@gmail.com>
2024-07-13 18:48:47 -0300

    Read file and create list

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 '''Little story about a character'''
+import os
+import time
 
-print("Hello world!")
-
-
-list_of_names = open("male.txt").read()
-
-'''Write here a code to compare male name.'''
-
-character = input("Please, enter a name:")
+# Function to create story
 
 def story(name):
     """Function that writes chapter one of story."""
+    time.sleep(10.0)
+    os.system("clear")
     print("""CHAPTER ONE""")
     print("""Everyone was happy in the kingdom of joy.""")
     print('''This kingdom was ruled by '''+ name + '''.''')
 
+#########################
 
-story(character)
+
+print("Hello world!")
+
+with open("male.txt") as file:
+    tuple_of_names = [tuple(line.split()) for line in file]
+
+character = input("Please, enter a name:")
+
+i = 0
+while i < len(tuple_of_names):
+    if character == tuple_of_names[i]:
+        print("NAME FOUND!")
+        story(character)
+        break
+    else:
+        i += 1
